6f404f8ad37e4233e6792776431b383a2989c650
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-04-05 00:09:13 +0100

    Added color to the text when the lamps are true or false, and also when you win or loose. added the 6 turn limit to the game

--- LampPuzzle/Program.cs
+++ LampPuzzle/Program.cs
@@ -12,10 +12,13 @@ namespace LampPuzzle
         public static bool lamp3 = false;
 
         public static bool puzzleDone = false;
+
+        public static int turns = 0;
+
     }
-    
+
     class Program
-    {   
+    {
         /// <summary>
         /// This method is used to switch the lamps states depending on the
         /// button the user selected. Button 1 switches lamp 1, button 2
@@ -24,21 +27,25 @@ namespace LampPuzzle
         /// <param name="button">Number of the button selected</param>
         static void SwitchLamp(int button)
         {
+
             if (button == 1)
             {
                 GV.lamp1 = !GV.lamp1;
+                GV.turns++;
             }
 
             if (button == 2)
             {
                 GV.lamp1 = !GV.lamp1;
                 GV.lamp2 = !GV.lamp2;
+                GV.turns++;
             }
 
             if (button == 3)
             {
                 GV.lamp2 = !GV.lamp2;
                 GV.lamp3 = !GV.lamp3;
+                GV.turns++;
             }
 
             if ((GV.lamp1 == true) && (GV.lamp2 == true) && (GV.lamp3 == true))
@@ -46,7 +53,8 @@ namespace LampPuzzle
                 GV.puzzleDone = true;
             }
         }
-        
+
+
         /// <summary>
         /// While the lamps are not all on, the method repeats, asking the user
         /// to choose the button to press and calling the SwitchLamp method to
@@ -58,33 +66,58 @@ namespace LampPuzzle
         /// used in the method</param>
         static void Main(string[] args)
         {
+            // Set color for "Lamp Puzzle"
+            // Yellow
+            Console.WriteLine("Welcome to" + "\u001b[33m" + " Lamp Puzzle!\n");
+
+            Console.WriteLine("\u001b[37m" + "In this game you have to turn "+
+            "on 3 lamps by pressing 3 buttons that will affect if they are "+ 
+            "on or off. You use the buttons " + "\u001b[34m" + " 1" + 
+            "\u001b[37m" + "," + "\u001b[34m" + " 2" + "\u001b[37m" + "," + 
+            "\u001b[34m" + " 3"+ "\u001b[37m" + " to change the " +
+            "lamps status and after each selection an update will be "
+            + "displayed on screen. Pressing 1 will change the first lamp, "+ 
+            "pressing 2 will change the first and second lamps, pressing 3 "+ 
+            "will changes the second and third lamps. Be careful, " +
+            "you only have " + "\u001b[31m" +
+             "6 " + "\u001b[37m" + "turns to complete this task. Good Luck!\n");
+            
+
             // Repeats while the lamps are not all on
-            while (GV.puzzleDone == false)
+            while (GV.puzzleDone == false && GV.turns < 6)
             {
                 int buttonPressed = 0;
 
-                Console.WriteLine("Welcome to Lamp Puzzle!\n");
-                Console.WriteLine("In this game you have to turn on 3 lamps "+ 
-                "by pressing 3 buttons that will affect if they are on or"+
-                " off. You use the buttons '1', '2' and '3' to change the "+
-                "lamps status and after each selection an update will be "
-                +"displayed on screen. Be careful, you only have 6 turns to "+
-                "complete this task. Good Luck!\n");
-
-                Console.WriteLine("Which button do you want to press?");
+                Console.WriteLine("\u001b[37m" + "Which button do you want to press?");
                 // User chooses the button to press
                 buttonPressed = int.Parse(Console.ReadLine());
 
                 // Calls SwitchLamp method passing the button the user chose
                 SwitchLamp(buttonPressed);
-
                 // Displays the state of every lamp
+                Console.Write(GV.lamp1 ? "\u001b[32m" : "\u001b[31m");
                 Console.WriteLine($"Lamp 1: {GV.lamp1}");
+                Console.Write(GV.lamp2 ? "\u001b[32m" : "\u001b[31m");
                 Console.WriteLine($"Lamp 2: {GV.lamp2}");
-                Console.WriteLine($"Lamp 3: {GV.lamp3}");
+                Console.Write(GV.lamp3 ? "\u001b[32m" : "\u001b[31m");
+                Console.WriteLine($"Lamp 3: {GV.lamp3}\n");
+                
+            }
+            // Displays the "You Loose!" message if the player reaches 6 turns
+            if (GV.turns == 6)
+            {
+                Console.Write("\u001b[31m");
+                Console.WriteLine("You loose!");
+            }
+            // Displays the "Congratulations!" message if the player 
+            //turns on all lamps in under 6 turns
+            else
+            {
+               
+                Console.Write("\u001b[32m");
+                Console.WriteLine("Congratulations!");
             }
 
-            Console.WriteLine("Congratulations!");
         }
     }
 }
